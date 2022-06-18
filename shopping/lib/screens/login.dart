cb20ee456545c99c5501b1fbdae1d30c51a53f36
Vicky Kumar Xaxa <vickykumar835302@gmail.com>
2022-06-18 11:33:46 +0530

    Created basic login page

--- shopping/lib/screens/login.dart
+++ shopping/lib/screens/login.dart
@@ -1,14 +1,80 @@
 // Login page
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 25,
+            ),
+            const Text(
+              'Enter Your Details',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _emailController,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter Your Email'),
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration:
+                        const InputDecoration(hintText: 'Enter Your Password'),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Column(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Login'),
+                      ),
+                      // const SizedBox(
+                      //   height: 15,
+                      // ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('Go to Sign Up'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
